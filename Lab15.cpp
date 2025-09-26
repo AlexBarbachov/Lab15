77d0ab44e7e51a82664551b821399a672b6edc28
AlexBarbachov <66849795+AlexBarbachov@users.noreply.github.com>
2025-09-26 15:48:52 -0700

    second

--- Lab15.cpp
+++ Lab15.cpp
@@ -1,10 +1,54 @@
 #include <iostream>
+#include <array>
+#include <string>
+
+using namespace std;
+
 
 class Movie
 {
     private:
+        string title;
+        int yearReleased;
+        string screenwriter;
 
     public:
+        // constructor.
+        Movie(string title, int year, string screenwriter)
+        {
+            title = title;
+            yearReleased = year;
+            screenwriter = screenwriter;
+        }
+
+        // setters & getters
+        void setTitle(string t) {
+
+        }
+
+        void setTitle(string t) {
+            
+        }
+
+        void setTitle(string t) {
+            
+        }
+
+        // getters
+        void getTitle(string t)
+        {
+
+        }
+
+        void getTitle(string t)
+        {
+            
+        }
+        
+        void getTitle(string t)
+        {
+            
+        }
 };
 
 int main()
